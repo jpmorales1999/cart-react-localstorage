fbe86918d6064d99bc8116395521d1a321237c72
Juan Pablo Morales <37224612+jpmorales1999@users.noreply.github.com>
2022-06-04 20:53:46 -0500

    Update Constants.js

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,4 +1,4 @@
 export const urlApiProducts = "http://myjson.dit.upm.es/api/bins/5dt9"
-export const BASE_PATH = 'mtcart.netlify.app'
+export const BASE_PATH = 'https://mtcart.netlify.app'
 export const STORAGE_PRODUCTS_CART = "productsCart"
 // https://myjson.dit.upm.es/5dt9
